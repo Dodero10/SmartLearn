2d0bd5e9fbdfc384067f3933e1c5301b94a7ed75
Dodero <truongdat.work@gmail.com>
2025-02-18 12:39:47 +0700

    fix bug

--- backend/constants/constants.py
+++ backend/constants/constants.py
@@ -3,3 +3,10 @@ import os
 CUR_DIRECTORY = os.path.dirname(os.path.realpath(__file__)).replace('\\', '/')
 SRC_DIRECTORY = os.path.dirname(CUR_DIRECTORY)
 DB_DIRECTORY = SRC_DIRECTORY + "/chromadb"
+
+BUCKET_NAME = "files"
+BUCKET_NAME_SLIDE = "slides"
+BUCKET_NAME_METADATA = "metadata"
+BUCKET_NAME_SCRIPTS = "scripts"
+BUCKET_NAME_AUDIO = "audio"
+BUCKET_NAME_VIDEO = "videos"

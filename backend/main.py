070307244de16dbfc313b3afca124fb119835b83
Nguyễn Hoàng Phúc (TTS KTCN) <tts_phucnh@viettelsoftware.com>
2025-02-18 23:06:34 +0700

    fix main.py Phuc

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ import io
 import logging
 import os
 from datetime import datetime
-
+from utils.database_manage import DatabaseManager
 from celery import chain
 from celery.result import AsyncResult
 from chat_query.query import gen_quiz, query
